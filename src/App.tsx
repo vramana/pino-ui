cde8b8229168890f8c300e06606826b2ac4e0845
Ramana Venkata <vramana@users.noreply.github.com>
2024-01-03 10:29:57 +0530

    Add UI for request response logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { get } from "idb-keyval";
+import { JsonEditor } from "json-edit-react";
+import omit from "lodash/omit";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fileHandle, setFileHandle] = useState<FileSystemFileHandle>();
+
+  const [logs, setLogs] = useState<unknown[]>([]);
+
+  const updateLogs = (contents: string) => {
+    const items = contents.split("\n");
+    items.reverse();
+    setLogs(
+      items
+        .map((item) => {
+          const log = item.trim() !== "" ? JSON.parse(item) : ({} as unknown);
+
+          if (log.level) {
+            const date = new Date(log.time).toString();
+
+            log.time = date;
+            log.url = `${log.baseURL}${log.url}`;
+          }
+          return omit(log, ["level", "pid", "hostname", "baseURL"]);
+        })
+        .slice(1),
+    );
+  };
+
+  const onClick = async () => {
+    const [handle] = await window.showOpenFilePicker();
+    const file = await handle.getFile();
+    setFileHandle(handle);
+    const contents = await file.text();
+    updateLogs(contents);
+  };
+
+  useEffect(() => {
+    const getFile = async () => {
+      const handle = await get("file");
+      if (handle) {
+        // setFileHandle(handle);
+      }
+    };
+    getFile();
+  }, []);
+
+  useEffect(() => {
+    const id = setInterval(async () => {
+      if (fileHandle) {
+        const file = await fileHandle.getFile();
+        const contents = await file.text();
+        updateLogs(contents);
+      }
+    }, 2000);
+    return () => clearInterval(id);
+  }, [fileHandle]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button type="button" onClick={onClick}>
+        Select File
+      </button>
+      {logs.map((log, index) => (
+        <JsonEditor
+          key={index}
+          data={log as object}
+          collapse={2}
+          restrictAdd
+          restrictEdit
+          restrictDelete
+        />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
